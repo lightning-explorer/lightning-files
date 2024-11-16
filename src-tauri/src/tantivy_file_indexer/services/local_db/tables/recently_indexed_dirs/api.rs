6d69d3e97fa38384bdd5330ab5260d82110fd75e
Grayson Rieger <graysonr12@icloud.com>
2024-11-16 09:01:55 -0600

    lets try

--- src-tauri/src/tantivy_file_indexer/services/local_db/tables/recently_indexed_dirs/api.rs
+++ src-tauri/src/tantivy_file_indexer/services/local_db/tables/recently_indexed_dirs/api.rs
@@ -7,34 +7,30 @@ pub struct RecentlyIndexedDirectoriesTable {
     pool: Arc<Pool<Sqlite>>,
 }
 
-impl RecentlyIndexedDirectoriesTable{
-
+impl RecentlyIndexedDirectoriesTable {
     pub async fn new_async(pool: Arc<Pool<Sqlite>>) -> Self {
         sqlx::query(
             "CREATE TABLE IF NOT EXISTS recent_indexed (
                     path TEXT PRIMARY KEY,
                     indexed_at INTEGER NOT NULL DEFAULT (strftime('%s', 'now'))
-                ) WITHOUT ROWID;" // last_modified INTEGER NOT NULL
+                ) WITHOUT ROWID;", // last_modified INTEGER NOT NULL
         )
         .execute(&*pool)
         .await
         .unwrap();
-        
+
         Self { pool }
     }
 
-    pub async fn insert(&self, model:RecentlyIndexedDirModel){
+    pub async fn insert(&self, model: RecentlyIndexedDirModel) {
         todo!();
     }
 
-    pub async fn check_if_recent(&self, dir_path:String)->bool{
+    pub async fn check_if_recent(&self, dir_path: String) -> bool {
         todo!();
         self.refresh();
         // refresh, then check
     }
-    
-    async fn refresh(&self){
-        
-    }
 
+    async fn refresh(&self) {}
 }
